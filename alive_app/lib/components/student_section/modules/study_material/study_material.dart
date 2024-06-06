961e67d97d92f5a53966139bfce4cb08594a653c
Yimkumer <yimkumerpongen@gmail.com>
2024-06-06 10:46:59 +0530

    updated study material section

--- alive_app/lib/components/student_section/modules/study_material/study_material.dart
+++ alive_app/lib/components/student_section/modules/study_material/study_material.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class StudyMaterial extends StatefulWidget {
   const StudyMaterial({super.key});
@@ -14,19 +15,27 @@ class _StudyMaterialState extends State<StudyMaterial> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
+        alignment: Alignment.center,
         children: <Widget>[
           // Form
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Container(
-              decoration: const BoxDecoration(
+              width: MediaQuery.of(context).size.width * 1,
+              height: MediaQuery.of(context).size.height * 0.3,
+              decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage('assets/bg.png'),
-                  fit: BoxFit.cover,
+                    image: const AssetImage('assets/book.jpg'),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.5), BlendMode.darken)),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
                 ),
               ),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   const Text(
                     'Study Materials',
@@ -39,39 +48,69 @@ class _StudyMaterialState extends State<StudyMaterial> {
                   const Text(
                     'Browse study materials here',
                     style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
-                    ),
+                        color: Colors.white,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500),
                   ),
                   const SizedBox(height: 16.0),
-                  DropdownButton<String>(
-                    dropdownColor: Colors.black,
-                    style: const TextStyle(color: Colors.white),
-                    items: <String>[
-                      'Acharya Institute Of Technology',
-                      'Acharya Institute of Graduate Studies',
-                      'Acharya Polytechnic',
-                      'Acharya & Bm Reddy College Of Pharmacy',
-                      'Acharyas Nrv School Of Architecture',
-                      'Smt.Nagarathnamma College Of Nursing',
-                      'Acharya School Of Design',
-                      'Acharya Institute Of Allied Health Sciences',
-                      'Smt. Nagarathnamma School Of Nursing',
-                      'Acharyas Nr institute Of Physiotherapy',
-                    ].map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        dropdownValue = newValue;
-                      });
-                    },
-                    hint: const Text(
-                      'Select Institute',
-                      style: TextStyle(color: Colors.white),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFE25A26),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButton<String>(
+                        style: const TextStyle(color: Colors.black),
+                        items: <String>[
+                          'Acharya Institute Of Technology',
+                          'Acharya Institute of Graduate Studies',
+                          'Acharya Polytechnic',
+                          'Acharya & Bm Reddy College Of Pharmacy',
+                          'Acharyas Nrv School Of Architecture',
+                          'Smt.Nagarathnamma College Of Nursing',
+                          'Acharya School Of Design',
+                          'Acharya Institute Of Allied Health Sciences',
+                          'Smt. Nagarathnamma School Of Nursing',
+                          'Acharyas Nr institute Of Physiotherapy',
+                        ].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            dropdownValue = newValue;
+                          });
+                        },
+                        hint: const Text(
+                          'Select Institute',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                        dropdownColor: Colors.white,
+                        elevation: 8,
+                        isExpanded: true,
+                        itemHeight: 50,
+                        iconEnabledColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search by subjects...',
+                        border: InputBorder.none,
+                        icon: Icon(Icons.search),
+                      ),
                     ),
                   ),
                 ],
